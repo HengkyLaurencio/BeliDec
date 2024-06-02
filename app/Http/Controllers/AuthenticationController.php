720f0dc12a3d6fe32f1b3102954aaa6eace3de69
BoviliusMeidi <boviliusbm@gmail.com>
2024-06-02 12:09:12 +0700

    authentication-controller login

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -4,20 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
 
 class AuthenticationController extends Controller
 {
-    public function register(){
+    public function register()
+    {
         return view("register");
     }
     public function registerPost(Request $request)
     {
         $request->validate([
-            'username' => 'required|string|max:255',
-            'email' => 'required|string|email|unique:users',
-            'password' => 'required|string|min:8',
+            'username' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'unique:users'],
+            'password' => ['required', 'string', 'min:8'],
         ]);
 
         $data['username'] = $request->username;
@@ -30,5 +33,23 @@ class AuthenticationController extends Controller
         }
 
         return redirect(route('login'))->with('success', 'Registration Success, login to access application');
+    }
+
+    public function loginPost(Request $request): RedirectResponse
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'string', 'email'],
+            'password' => ['required', 'string', 'min:8'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('home');
+        }
+
+        return back()->withErrors([
+            'email' => 'Email atau Password ada yang tidak sama!',
+        ])->onlyInput('email');
     }
 }
