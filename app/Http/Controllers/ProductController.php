529612950b9268ce87299224516a0f80a206a0b5
hanseehhh <mrhns00@gmail.com>
2024-06-02 12:12:32 +0700

    make getProduct

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Product;
 
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function getProduct() {
+        $productData = Product::all();
+        return view();
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/getProduct', [ProductController::class, 'getProduct'])->name('getProduct');
