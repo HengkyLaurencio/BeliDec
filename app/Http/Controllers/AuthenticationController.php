c5239fa2238a729507f0801d42a4d3c4ab1bc05b
BoviliusMeidi <boviliusbm@gmail.com>
2024-06-02 10:40:55 +0700

    authentication-controller register

---
+++ app/Http/Controllers/AuthenticationController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthenticationController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:8|',
+        ]);
+
+        $data['name'] = $request->name;
+        $data['email'] = $request->email;
+        $data['password'] = Hash::make($request->password);
+        $user = User::create($data);
+
+        if (!$user) {
+            return redirect(route('registration'))->with('error','Registration Failed, try again.');
+        }
+
+        return redirect(route('login'))->with('success','Registration Success, login to access application');
+    }
+}
