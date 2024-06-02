bc9c7fd5576e19e11bf518c96945c45d8580ff64
BoviliusMeidi <boviliusbm@gmail.com>
2024-06-02 09:06:37 +0700

    fix something missing

--- database/migrations/2024_05_29_152303_create_shops_table.php
+++ database/migrations/2024_05_29_152303_create_shops_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('shops', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->unsignedBigInteger('owner_id');
             $table->timestamps();
 
