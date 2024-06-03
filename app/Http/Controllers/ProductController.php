a785f1eef3ddd17bd31e98ea702485df42e40336
hanseehhh <mrhns00@gmail.com>
2024-06-03 10:51:38 +0700

    make createProduct function

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\Products;
 use App\Models\Product;
 
 use Illuminate\Http\Request;
@@ -24,6 +23,8 @@ class ProductController extends Controller
                             <th>Product</th>
                             <th>Desc</th>
                             <th>Price</th>
+                            <th>Stock</th>
+                            <th>ShopId</th>
                         </tr>';
                 
                 foreach ($productData as $product) {
@@ -32,6 +33,8 @@ class ProductController extends Controller
                             <td>' . $product->name . '</td>
                             <td>' . $product->description . '</td>
                             <td>' . $product->price . '</td>
+                            <td>' . $product->stock . '</td>
+                            <td>' . $product->shop_id . '</td>
                             </tr>';
                 }
 
@@ -43,6 +46,9 @@ class ProductController extends Controller
 
     public function getProducts($id){
         $product = Product::find($id);
+        if (!$product) {
+            return response('Product not found');
+        }
         echo '<!DOCTYPE html>
         <html>
             <head>
@@ -71,8 +77,75 @@ class ProductController extends Controller
         </html>';
     }
 
-    public function createProduct(){
-        
-        
+    public function simpan(Request $request) {
+        $productData = $request->validate([
+            'productName' => ['required', 'string'],
+            'Description' => ['required', 'string'],
+            'Price' => ['required', 'decimal:2'],
+            'Stock' => ['required', 'integer'],
+            'ShopId' => ['required', 'integer'],
+        ]);
+    
+        $newProduct = Product::create([
+            'name' => $productData['productName'],
+            'description' => $productData['Description'],
+            'price' => $productData['Price'],
+            'stock' => $productData['Stock'],
+            'shop_id' => $productData['ShopId'],
+        ]);
+    
+        return redirect()->route('getProduct');
     }
+    
+    public function createProduct(Product $product, Request $request){
+        return '
+        <html>
+            <head><title>Tes</title>
+            <body>
+            <h1>Create Product</h1>
+                <form method="post" action="'.route('simpan').'">
+                    <input type="hidden" name="_token" value="' . csrf_token() . '">
+                    <div>
+                    <label>Product Name</label>
+                    <input type="text" name="productName" placeholder="Product Name">
+                    </div>
+                    <div>
+                    <label>Description</label>
+                    <input type="text" name="Description" placeholder="Description">
+                    </div>
+                    <div>
+                    <label>Price</label>
+                    <input type="text" name="Price" placeholder="Price">
+                    </div>
+                    <div>
+                    <label>Stock</label>
+                    <input type="text" name="Stock" placeholder="Stock">
+                    </div>
+                    <div>
+                    <label>ShopId</label>
+                    <input type="text" name="ShopId" placeholder="ShopId">
+                    </div>
+                    <div>
+                    <input type="submit" value="Create Product">
+                    </div>
+                </form>
+            </body>
+        </html>
+        ';
+    }
+    
+
+    // public function putProduct(Product $product, Request $request ){
+    //     $productData = $request->validate([
+    //         'id' => 'required',
+    //         'name' => 'required | string',
+    //         'description' => 'required | string',
+    //         'price' => 'required | decimal:2'
+    //     ]);
+
+    //     $product->update($productData);
+
+    //     return redirect(route('getProduct'));
+
+    // }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,4 +10,7 @@ Route::get('/', function () {
 Route::get('/Product', [ProductController::class, 'getProduct'])->name('getProduct');
 Route::get('/Product/{id}',  [ProductController::class, 'getProducts'])->name('getProducts');
 
-Route::get('/Product/Create', [ProductController::class, 'createProduct'])->name('getProduct');
+Route::get('/CreateProduct',  [ProductController::class, 'createProduct'])->name('createProduct');
+Route::post('/Simpan',  [ProductController::class, 'simpan'])->name('simpan');
+
+// Route::put('/Product/{id}/updateProduct', [ProductController::class, 'putProduct'])->name('updateProduct');
