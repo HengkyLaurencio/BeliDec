48e0a0d7c91aa19458037997df171b8510bd5162
louisfebriano <louisfebriano69@gmail.com>
2024-06-18 21:10:48 +0700

    edited review models

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class Review extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'user_id',
+        'product_order_id',
+        'stars',
+        'comments',
+    ];
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
 }
