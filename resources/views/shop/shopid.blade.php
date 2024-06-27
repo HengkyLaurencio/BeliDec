177690b8602dd6ce188a930579cbe7b5b4c8497a
BoviliusMeidi <boviliusbm@gmail.com>
2024-06-27 20:59:48 +0700

    fix : ui in the shop id view

--- resources/views/shop/shopid.blade.php
+++ resources/views/shop/shopid.blade.php
@@ -5,16 +5,16 @@
 
     <body class="bg-white dark:bg-primary-dark dark:text-text-dark">
             @include('layouts.header')
-            
-        <section class="text-gray-700 body-font overflow-hidden bg-white"> 
+
+        <section class="text-gray-700 body-font overflow-hidden bg-white">
         <div class="container max-w-l p-6 mx-auto space-y-12">
-            <div>
+            <div class="mx-28">
                 <h2 class="text-3xl font-bold text-left sm:text-4xl">{{ $shopData -> name}}</h2>
                 <p class="text-xl text-left">{{ $shopData -> description}}</p>
                 <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5"></div>
             </div>
                 <div class="px-28">
-                <div class="mt-6 grid grid-cols-4 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8"> 
+                <div class="mt-6 grid grid-cols-4 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                 @foreach($productData as $product)
                 <div class="group relative">
                     <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
@@ -28,7 +28,7 @@
                                 {{$product -> name}}
                                 </a>
                             </h3>
-                            
+
                             </div>
                             <p class="text-sm font-medium text-gray-900">Rp {{ number_format($product->price, 0, ',', '.') }}</p>
                         </div>
